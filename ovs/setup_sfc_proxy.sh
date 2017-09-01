8ae229f5f9348f8d1717852466137a2c00449cc2
Miguel Angel Munoz Gonzalez <magonzalez@fortinet.com>
2017-09-01 19:26:04 +0200

    Avoid unnecessary nohups

--- ovs/setup_sfc_proxy.sh
+++ ovs/setup_sfc_proxy.sh
@@ -6,11 +6,12 @@ apt-get update
 apt-get install -y python3-pip
 pip3 install hexdump
 
-pkill proxy
+pkill proxy.py
 pkill tcpdump
 
-nohup  tcpdump -i eth1 -l >/dev/null &
+tcpdump -i eth1 -l >/dev/null &
 
-nohup tcpdump -i eth2 -l >/dev/null &
+tcpdump -i eth2 -l >/dev/null &
 
-nohup /vagrant/proxy.py --encap_if eth0 --unencap_in_if eth2 --unencap_out_if eth1 2>&1 >proxy.log &
+/vagrant/proxy.py --encap_if eth0 --unencap_in_if eth2 --unencap_out_if eth1 2>&1 >proxy.log &
+

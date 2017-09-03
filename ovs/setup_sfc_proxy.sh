11d8d7209c5955dd28564f003d0a75c48b7b5e47
Miguel Angel Munoz Gonzalez <magonzalez@fortinet.com>
2017-09-03 16:34:39 +0200

    Fixed MTU issue when FGT accumulates packets and sends bigger than 1500

--- ovs/setup_sfc_proxy.sh
+++ ovs/setup_sfc_proxy.sh
@@ -13,5 +13,18 @@ tcpdump -i eth1 -l >/dev/null &
 
 tcpdump -i eth2 -l >/dev/null &
 
+ethtool -K eth1 tx off
+
+ethtool -K eth2 tx off
+
+ifconfig eth0 promisc
+ifconfig eth1 promisc
+ifconfig eth2 promisc
+
+ifconfig eth0 mtu 4096
+ifconfig eth1 mtu 4096
+ifconfig eth2 mtu 4096
+
+
 /vagrant/proxy.py --encap_if eth0 --unencap_in_if eth2 --unencap_out_if eth1 2>&1 >proxy.log &
 

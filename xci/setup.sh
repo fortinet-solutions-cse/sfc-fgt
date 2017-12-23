6024c178eac8cf79e2ef42cd3b73450a6f758b1d
Miguel Angel Munoz <magonzalez@fortinet.com>
2017-12-23 20:34:42 +0100

    Allow inet access from VM in XCI

--- xci/setup.sh
+++ xci/setup.sh
@@ -33,6 +33,15 @@ openstack security group rule create --proto udp --dst-port 1:65535  --ingress d
 openstack security group rule create --proto tcp --dst-port 1:65535  --egress default || echo "should have been created already"
 openstack security group rule create --proto udp --dst-port 1:65535  --egress default || echo "should have been created already"
 
+#Iptables to allow Inet Access from VMs
+
+iptables -F
+iptables -F -t nat
+iptables -P FORWARD -j ACCEPT
+iptables -A POSTROUTING -t nat -j MASQUERADE
+
+#Note interface has to be set to promiscuous mode to allow inet traffic in the vms
+ifconfig br-vlan promisc
 
 # Create networks
 
@@ -43,7 +52,6 @@ openstack router show router-ext||openstack router create router-ext
 openstack router add subnet router-ext mgmt_subnet
 openstack router set --external-gateway ext-net router-ext
 
-
 # Get images
 
 wget http://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-amd64-disk1.img
@@ -51,7 +59,8 @@ sudo apt install -y libguestfs-tools
 
 sudo virt-sysprep -a trusty-server-cloudimg-amd64-disk1.img --root-password password:m \
     --delete /var/lib/cloud/* \
-    --firstboot-command 'useradd -m -p "" m ; chage -d 0 m; ssh-keygen -A; rm -rf /var/lib/cloud/*;'
+    --firstboot-command 'useradd -m -p "" m ; chage -d 0 m; ssh-keygen -A; rm -rf /var/lib/cloud/*;sed -i "s/PermitRootLogin .*/PermitRootLogin yes/" /etc/ssh/sshd_config; sed -i "s/PasswordAuthentication .*/PasswordAuthentication yes/" /etc/ssh/sshd_config;sed -i "s/nameserver .*/nameserver 8.8.8.8/" /etc/resolv.conf;"
+'
 
 add "sed" for ssh configuration: root login and password login
 
@@ -146,7 +155,17 @@ do
   retries=$((retries-1))
 done
 
-ssh $sshopts -i t1.pem ubuntu@$floatIpClient "sudo apt install -y openvswitch-switch;" \
+#Review security groups again. They are created twice
+openstack security group list|grep default|awk '{print $2}'|xargs -I[] openstack --insecure security group delete []
+
+openstack security group rule create --proto tcp --dst-port 1:65535  --ingress default || echo "should have been created already"
+openstack security group rule create --proto udp --dst-port 1:65535  --ingress default || echo "should have been created already"
+openstack security group rule create --proto tcp --dst-port 1:65535  --egress default || echo "should have been created already"
+openstack security group rule create --proto udp --dst-port 1:65535  --egress default || echo "should have been created already"
+
+
+
+ssh $sshopts -i t1.pem ubuntu@$floatIpClient "sudo apt update; sudo apt install -y openvswitch-switch;" \
  "sudo ovs-vsctl add-br br-sfc -- set bridge br-sfc protocols=OpenFlow10,OpenFlow12,OpenFlow13;" \
  "sudo ovs-vsctl add-port br-sfc eth1"
 
@@ -156,3 +175,132 @@ openstack sfc --insecure port pair create --ingress=${pServerMid} --egress=${pSe
 
 openstack sfc --insecure port pair group create --port-pair ppClientM pgClientM
 openstack sfc --insecure port pair group create --port-pair ppServerM pgServerM
+
+
+exit 0
+
+# Final status of iptables in opnfv vm:
+
+(openstack) (nwsfc) root@opnfv:~# iptables -S
+-P INPUT ACCEPT
+-P FORWARD ACCEPT
+-P OUTPUT ACCEPT
+-N DOCKER
+-N DOCKER-ISOLATION
+(nwsfc) root@opnfv:~# iptables -L
+Chain INPUT (policy ACCEPT)
+target     prot opt source               destination
+
+Chain FORWARD (policy ACCEPT)
+target     prot opt source               destination
+
+Chain OUTPUT (policy ACCEPT)
+target     prot opt source               destination
+
+Chain DOCKER (0 references)
+target     prot opt source               destination
+
+Chain DOCKER-ISOLATION (0 references)
+target     prot opt source               destination
+(nwsfc) root@opnfv:~#
+
+
+
+
+
+
+(nwsfc) root@opnfv:~# iptables -S -t nat
+-P PREROUTING ACCEPT
+-P INPUT ACCEPT
+-P OUTPUT ACCEPT
+-P POSTROUTING ACCEPT
+-N DOCKER
+-A POSTROUTING -j MASQUERADE
+(nwsfc) root@opnfv:~# iptables -V -t nat
+iptables v1.6.0
+(nwsfc) root@opnfv:~# iptables -L -t nat
+Chain PREROUTING (policy ACCEPT)
+target     prot opt source               destination
+
+Chain INPUT (policy ACCEPT)
+target     prot opt source               destination
+
+Chain OUTPUT (policy ACCEPT)
+target     prot opt source               destination
+
+Chain POSTROUTING (policy ACCEPT)
+target     prot opt source               destination
+MASQUERADE  all  --  anywhere             anywhere
+
+Chain DOCKER (0 references)
+target     prot opt source               destination
+(nwsfc) root@opnfv:~#
+
+
+
+
+
+(nwsfc) root@opnfv:~# iptables -S -t filter
+-P INPUT ACCEPT
+-P FORWARD ACCEPT
+-P OUTPUT ACCEPT
+-N DOCKER
+-N DOCKER-ISOLATION
+(nwsfc) root@opnfv:~# iptables -L -t filter
+Chain INPUT (policy ACCEPT)
+target     prot opt source               destination
+
+Chain FORWARD (policy ACCEPT)
+target     prot opt source               destination
+
+Chain OUTPUT (policy ACCEPT)
+target     prot opt source               destination
+
+Chain DOCKER (0 references)
+target     prot opt source               destination
+
+Chain DOCKER-ISOLATION (0 references)
+target     prot opt source               destination
+(nwsfc) root@opnfv:~#
+
+
+
+
+
+
+(nwsfc) root@opnfv:~# iptables -S -t raw
+-P PREROUTING ACCEPT
+-P OUTPUT ACCEPT
+(nwsfc) root@opnfv:~# iptables -L -t raw
+Chain PREROUTING (policy ACCEPT)
+target     prot opt source               destination
+
+Chain OUTPUT (policy ACCEPT)
+target     prot opt source               destination
+(nwsfc) root@opnfv:~#
+
+(nwsfc) root@opnfv:~# iptables -S -t mangle
+-P PREROUTING ACCEPT
+-P INPUT ACCEPT
+-P FORWARD ACCEPT
+-P OUTPUT ACCEPT
+-P POSTROUTING ACCEPT
+(nwsfc) root@opnfv:~# iptables -L -t mangle
+Chain PREROUTING (policy ACCEPT)
+target     prot opt source               destination
+
+Chain INPUT (policy ACCEPT)
+target     prot opt source               destination
+
+Chain FORWARD (policy ACCEPT)
+target     prot opt source               destination
+
+Chain OUTPUT (policy ACCEPT)
+target     prot opt source               destination
+
+Chain POSTROUTING (policy ACCEPT)
+target     prot opt source               destination
+(nwsfc) root@opnfv:~#
+
+
+

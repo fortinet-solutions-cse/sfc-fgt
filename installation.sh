c862c23e278884499666e62989f0c150f0038639
Miguel Angel Munoz <magonzalez@fortinet.com>
2018-03-12 21:55:15 +0100

    SFC installation instructions

--- installation.sh
+++ installation.sh
@@ -13,3 +13,10 @@ sudo apt-get install -y virt-manager
 
 # Things done
 # Check this: set etc/sysctl.conf: ipv4.forward (uncomment)
+echo -e "\n\n*****************************************************"
+echo -e "Running ssh-keygen... please enter fields manually:"
+echo -e "*****************************************************"
+
+ssh-keygen
+
+echo -e "Done. \n\nPlease REBOOT NOW before starting demo:\n\n sudo reboot\n"

5b8fe0f6eb40c8cfb85fb827d8e60e566d98ba7c
root <magonzalez@fortinet.com>
2018-08-07 13:44:11 +0000

    New status script

---
+++ opnfv_plugtests_nsh/status.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -x
+
+
+tacker vnffg-list
+
+tacker vnffgd-list
+
+tacker vnf-list
+
+tacker vnfd-list
+
+tacker vim-list
+
+openstack image list
+
+openstack server list
+
+openstack flavor list
+
+openstack subnet list
+
+openstack network list
+
+openstack router list
+
+openstack port list
